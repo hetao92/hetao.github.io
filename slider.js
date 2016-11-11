66990345272fc1ca7f3ea9abcd50f8e58bc6548a
hetao <hetao.daily@gmail.com>
2016-11-11 17:22:13 +0800

    project add
    Todo List/Slider/Player/Weibo

---
+++ slider.js
@@ -0,0 +1,75 @@
+//绑定按钮
+var play = function(num) {
+    var activeIndex = $('.imgContainer').data('active')
+    var numberOfImgs = $('.imgContainer').data('imgs')
+    var i = (activeIndex + numberOfImgs + num) % numberOfImgs
+    $('.imgContainer').data('active',i)
+    $('.img-slide-active').fadeOut()
+    $('.img-slide-active').removeClass('img-slide-active')
+    var active = $($('.img-slide')[i])
+    active.addClass('img-slide-active')
+    active.fadeIn()
+//指示器
+    $('.indicator-slide-active').removeClass('indicator-slide-active')
+    var activeIndicator = $($('.indicator-slide')[i])
+    activeIndicator.addClass('indicator-slide-active')
+}
+
+$('.img-button').on('click', function(event) {
+    var button = $(event.target)
+    if (button.hasClass('img-button-left')) {
+        play(-1)
+    } else {
+        play(1)
+    }
+})
+//鼠标点击播放图片
+$('.indicator-slide').on('click', function(event) {
+    console.log('1')
+    var button = $(event.target)
+    var i = $(event.target).data('index')
+
+    $('.imgContainer').data('active',i)
+    $('.img-slide-active').fadeOut()
+    $('.img-slide-active').removeClass('img-slide-active')
+
+    var active = $($('.img-slide')[i])
+    active.addClass('img-slide-active')
+    active.fadeIn()
+
+    $('.indicator-slide-active').removeClass('indicator-slide-active')
+    button.addClass('indicator-slide-active')
+})
+
+//鼠标滑过播放
+// $('.indicator-slide').on('mouseover', function(event) {
+//     var button = $(event.target)
+//     var i = $(event.target).data('index')
+//
+//     $('.imgContainer').data('active',i)
+//     $('.img-slide-active').fadeOut()
+//     $('.img-slide-active').removeClass('img-slide-active')
+//
+//     var active = $($('.img-slide')[i])
+//     active.addClass('img-slide-active')
+//     active.fadeIn()
+//
+//     $('.indicator-slide-active').removeClass('indicator-slide-active')
+//     button.addClass('indicator-slide-active')
+// })
+
+
+//设置按钮显现
+var s = `
+<style class='button-hover'>
+.img-button{
+    opacity:0.7;
+}
+`
+$('.slideContainer').on('mouseover', function() {
+    $('style').append(s)
+})
+$('.slideContainer').on('mouseout', function() {
+    $('.button-hover').fadeOut()
+    $('.button-hover').remove()
+})
---
+++ weibo.js
@@ -0,0 +1,35 @@
+$('.message').on('input',function(){
+    var len = $('.message').val().length
+    $('.wordLimit').text(140 - len)
+})
+//数字变化
+// $('.message').on('keyup',function() {
+//     console.log($('.message').val())
+//     var len = $('.message').val().length
+//     $('.wordLimit').text(140 - len)
+// })
+//绑定按钮
+$('.submitButton').on('click', function() {
+    var content = $('.message').val()
+    var month = new Date().getMonth() + 1
+    var day = new Date().getDate()
+    var hour = new Date().getHours()
+    var seconds = new Date().getSeconds()
+    var s = `
+        <div class="content">
+            <div class='reply'>
+                <p class="replyContent">核桃:${content}</p>
+                <p class='replyDate'>${month}月${day}日 ${hour}:${seconds}</p>
+            </div>
+            <img src="img/qiu.jpeg" class='replyIcon' alt="" />
+        </div>
+    `
+    if (content.length <= 140) {
+        $('.replyContainer').append(s)
+        $('.message').val('')
+        $('.wordLimit').text(140)
+    }else {
+        alert('您输入的字数已超限制，请重新输入！')
+    }
+
+})
